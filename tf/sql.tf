44229c80a3f4c775b7b40cf58ca9a472d3ee5e69
Joe Heaton.dev <joe@heaton.dev>
2022-10-26 12:53:09 +0100

    cloud sql (#5)

--- tf/sql.tf
+++ tf/sql.tf
@@ -1,10 +1,12 @@
-module "psql" {
-  source           = "./fabric/modules/cloudsql-instance"
-  project_id       = local.vars.project
-  network          = module.vpc.network.self_link
-  name             = "psql-${local.vars.name}"
-  region           = local.vars.region
-  database_version = local.vars.sql.version
-  tier             = local.vars.sql.tier
-  flags            = local.vars.sql.flags
+module "sql" {
+  for_each          = local.vars.sql
+  source            = "./fabric/modules/cloudsql-instance"
+  project_id        = local.vars.project
+  network           = module.vpc.network.self_link
+  name              = "sql-${each.key}-${local.suffix}"
+  region            = local.vars.region
+  database_version  = local.vars.sql[each.key].version
+  tier              = local.vars.sql[each.key].tier
+  availability_type = local.vars.sql[each.key].availability_type
+  flags             = local.vars.sql[each.key].flags
 }
